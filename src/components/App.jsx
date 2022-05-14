2d25f66ea30904b8ce41f2c5e0a5609aee20b4ca
elinamakarenko <68436291+elinamakarenko@users.noreply.github.com>
2022-05-14 22:32:06 +0300

    finish commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { Component } from 'react';
-import Feedback from './Feedback';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Notification from './Notification';
 
 class App extends Component {
   state = {
@@ -10,33 +13,67 @@ class App extends Component {
 
   incrementFeedback = value => {
     console.log(value);
-      switch (value) {
-        case 'good':
-          this.setState(prevState => ({
-            good: prevState.good + 1,
-          }));
-          break;
-        case 'neutral':
-          this.setState(prevState => ({
-            neutral: prevState.neutral + 1,
-          }));
-          break;
-        case 'bad':
-          this.setState(prevState => ({
-            bad: prevState.bad + 1,
-          }));
-          break;
-        default:
-          break;
+    switch (value) {
+      case 'good':
+        this.setState(prevState => ({
+          good: prevState.good + 1,
+        }));
+        break;
+      case 'neutral':
+        this.setState(prevState => ({
+          neutral: prevState.neutral + 1,
+        }));
+        break;
+      case 'bad':
+        this.setState(prevState => ({
+          bad: prevState.bad + 1,
+        }));
+        break;
+      default:
+        break;
     }
   };
 
+  countTotalFeedback = countTotal => {
+    const values = Object.values(this.state);
+    for (const value of values) {
+      countTotal += value;
+    }
+    return countTotal;
+  };
+
+  countPositiveFeedbackPercentage = (countPositive, countTotal) => {
+    const positive = this.state.good;
+    if (countTotal) {
+      countPositive = Math.round((positive / countTotal) * 100);
+    }
+    return countPositive;
+  };
+
   render() {
-    const feedback = this.state;
+    const countTotal = 0;
+    const countPositive = 0;
+
     return (
-      <div>
-        <Feedback feedback={feedback} addFeedback={this.incrementFeedback} />
-      </div>
+      <>
+        <Section>
+          <FeedbackOptions addFeedback={this.incrementFeedback} />
+        </Section>
+        <Section>
+          {this.countTotalFeedback(countTotal) ? (
+            <Statistics
+              feedback={this.state}
+              total={this.countTotalFeedback(countTotal)}
+              positiveFeedback={this.countPositiveFeedbackPercentage(
+                countPositive,
+                this.countTotalFeedback(countTotal)
+              )}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </>
     );
   }
 }
